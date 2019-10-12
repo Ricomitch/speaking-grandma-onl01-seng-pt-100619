9ee3fe58100b3adf935051d7174214e5e2c7cc23
Rico Mitchell <ricomitch@icloud.com>
2019-10-12 00:18:00 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma("phrase")
   
-    if phrase = "I LOVE YOU GRANDMA!"
-    return = "I LOVE YOU TOO PUMPKIN!"
+    if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
     
-    elsif
+    elsif 
     else
 
 end
