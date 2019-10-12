6f8121d6f364a0763caf9ad09b025ae553978d1e
Rico Mitchell <ricomitch@icloud.com>
2019-10-12 00:34:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,13 +2,11 @@
 def speak_to_grandma("phrase")
   
   my_phrase = ()
-    if phrase == "I LOVE YOU GRANDMA!"
+  if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-    
-    elsif phrase == "phrase".upcase
+  elsif phrase == "phrase".upcase
     return NO, NOT SINCE 1938!
-    
-    else
+  else
     return HUH?! SPEAK UP, SONNY! 
   end
   my_phrase
